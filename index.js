f01674ee5b7ee97527b9c8957ec24bd573fddde0
Tanvvee Mantri <gamingtanvee5@gmail.com>
2026-07-15 22:02:07 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import "./styles/Webinar.css";   // global styles
-import "./styles/variables.css"; // CSS variables
+<!-- Load React and ReactDOM from CDN -->
+<script src="https://unpkg.com/react@18/umd/react.development.js"></script>
+<script src="https://unpkg.com/react-dom@18/umd/react-dom.development.js"></script>
+<script src="https://unpkg.com/@babel/standalone/babel.min.js"></script>
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  React.createElement("div", { style: { fontFamily: "Arial", textAlign: "center", marginTop: "50px" } },
-    React.createElement("h1", null, "Welcome to Webinar App"),
-    React.createElement("p", null, "Register and join your session instantly!")
-  )
-);
+<script type="text/babel">
+  function App() {
+    return (
+      <div style={{ fontFamily: "Arial", textAlign: "center", marginTop: "50px" }}>
+        <h1>Welcome to Webinar App</h1>
+        <p>Register and join your session instantly!</p>
+      </div>
+    );
+  }
+
+  const root = ReactDOM.createRoot(document.getElementById("root"));
+  root.render(<App />);
+</script>
