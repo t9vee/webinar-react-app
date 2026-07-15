fbc8684e03d149f20233ab10a3b1af1955cc722c
Tanvvee Mantri <gamingtanvee5@gmail.com>
2026-07-15 20:55:55 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,13 @@
+<script type="text/babel">
+  function App() {
+    return (
+      <div style={{ fontFamily: "Arial", textAlign: "center", marginTop: "50px" }}>
+        <h1>Welcome to Webinar App</h1>
+        <p>Register and join your session instantly!</p>
+      </div>
+    );
+  }
+
+  const root = ReactDOM.createRoot(document.getElementById("root"));
+  root.render(<App />);
+</script>
