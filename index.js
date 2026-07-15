460eac24b5f09236369da049c419976cadc46c7a
Tanvvee Mantri <gamingtanvee5@gmail.com>
2026-07-15 22:03:57 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,11 @@
-<!-- Load React and ReactDOM from CDN -->
-<script src="https://unpkg.com/react@18/umd/react.development.js"></script>
-<script src="https://unpkg.com/react-dom@18/umd/react-dom.development.js"></script>
-<script src="https://unpkg.com/@babel/standalone/babel.min.js"></script>
+function App() {
+  return (
+    <div style={{ fontFamily: "Arial", textAlign: "center", marginTop: "50px" }}>
+      <h1>Welcome to Webinar App</h1>
+      <p>Register and join your session instantly!</p>
+    </div>
+  );
+}
 
-<script type="text/babel">
-  function App() {
-    return (
-      <div style={{ fontFamily: "Arial", textAlign: "center", marginTop: "50px" }}>
-        <h1>Welcome to Webinar App</h1>
-        <p>Register and join your session instantly!</p>
-      </div>
-    );
-  }
-
-  const root = ReactDOM.createRoot(document.getElementById("root"));
-  root.render(<App />);
-</script>
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
