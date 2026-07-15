ee14756476885130ad56117b2877ff670ec4f5b9
Tanvvee Mantri <gamingtanvee5@gmail.com>
2026-07-15 19:53:15 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import WebinarPage from "./components/WebinarPage"; // main page component
+
+function App() {
+  return (
+    <div className="App">
+      <WebinarPage />
+    </div>
+  );
+}
+
+export default App;
